c47a8341cb25a4877bbd99f376f52fc066e53e4b
Alex Fitiskin <alexfitiskin@gmail.com>
2020-05-15 15:10:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# `recoil-form`: blazing fast React forms build on top of Recoil
+
+## Installation
+```
+npm install --save recoil-form recoil
+// or
+yarn add recoil-form recoil
+```
+
+## Work in progress
+
+This project is currently in "Work in progress" status
+
+## API and Usage
+
+API and Usage guides is a subject to change
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,42 @@
+const useForm = () => {
+  throw new Error('Sorry, `useForm` hook is not implemented yet!');
+}
+
+const useField = () => {
+  throw new Error('Sorry, `useField` hook is not implemented yet!');
+}
+
+const useFieldArray = () => {
+  throw new Error('Sorry, `useFieldArray` hook is not implemented yet!');
+}
+
+const useErrorMessage = () => {
+  throw new Error('Sorry, `useErrorMessage` hook is not implemented yet!');
+}
+
+const Form = () => {
+  throw new Error('Sorry, `Form` component is not implemented yet!');
+}
+
+const Field = () => {
+  throw new Error('Sorry, `Field` component is not implemented yet!');
+}
+
+const FieldArray = () => {
+  throw new Error('Sorry, `Field` component is not implemented yet!');
+}
+
+const ErrorMessage = () => {
+  throw new Error('Sorry, `ErrorMessage` component is not implemented yet!');
+}
+
+module.exports = {
+  useForm,
+  useField,
+  useFieldArray,
+  useErrorMessage,
+  Form,
+  Field,
+  FieldArray,
+  ErrorMessage,
+};
